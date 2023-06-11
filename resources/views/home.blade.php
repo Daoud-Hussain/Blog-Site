ab66f5dc3a13156befb913c486b468c85bc9beae
Daoud-Hussain <daoudhussain302@gmail.com>
2023-06-11 11:34:22 +0500

    Creating a home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,32 @@
+<x-app-layout>
+    <div class="py-12">
+        <h2 class=" text-center mb-4">Blogging Site</h2>
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="container p-2">
+                
+                    <table class="table table-white ">
+                        <thead>
+                        <tr>
+                            <th scope="col">Name</th>
+                            <th scope="col">Title</th>
+                            <th scope="col">Content</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach ($posts as $post)
+                            <tr>
+                                <td>{{$post->user->name}}</td>
+                                <td>{{$post->title}}</td>
+                                <td >{{$post->body}}</td>
+                            </tr>
+                           
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,9 +17,6 @@ use App\Http\Controllers\DashboardController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 
 Route::middleware('auth')->group(function () {
@@ -26,16 +24,18 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-    Route::get('/dashboard', [DashboardController::class, 'show_posts'])->middleware(['auth', 'verified'])->name('dashboard');
+    
+    Route::get('/', [HomeController::class, 'show_posts'])->name('home');
+    Route::get('/home', [HomeController::class, 'show_posts'])->name('home');
     
     //Route for creating post
+    Route::get('/dashboard', [DashboardController::class, 'show_posts'])->middleware(['auth', 'verified'])->name('dashboard');
     Route::get('/post', [PostController::class, 'index'])->name('post_index');
     Route::post('/post', [PostController::class, 'create'])->name('post_create');
     Route::get('/post/edit/{id}', [PostController::class, 'edit'])->name('post_edit');
     Route::put('/post/edit/{id}', [PostController::class, 'update'])->name('post_update');
     Route::get('/post/delete/{id}', [PostController::class, 'destroy'])->name('post_destroy');
-
+    
 });
 
 
